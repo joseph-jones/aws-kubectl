f36d2209b5fa4acfd1750b2467ad5ed8fb289d00
Joseph Jones <the.tuxster@gmail.com>
2021-07-17 10:15:40 -0500

    more changes

--- rotate-access-key.sh
+++ rotate-access-key.sh
@@ -10,7 +10,12 @@ trap "rm -f ${TMP_FILE}" EXIT TERM INT
 
 
 # remove any inactive keys
-INACTIVE=$(aws iam list-access-keys | jq -r '.AccessKeyMetadata[] | select(.Status != "Active") | .AccessKeyId')
+INACTIVE=$(aws iam list-access-keys | jq -r '.AccessKeyMetadata[] | select(.Status == "Inactive") | .AccessKeyId')
+ACTIVE=$(aws iam list-access-keys | jq -r '.AccessKeyMetadata[] | select(.Status == "Active") | .AccessKeyId')
+
+echo "Active Key ${ACTIVE:-}"
+echo "Inactive Key ${INACTIVE:-}"
+
 if [[ -n ${INACTIVE:-}  ]]; then
     echo "remove inactive access keys"
     aws iam delete-access-key --access-key-id $INACTIVE
@@ -26,11 +31,23 @@ kubectl create secret generic aws-secrets-old \
     --from-literal=aws-secret-access-key=${AWS_SECRET_ACCESS_KEY}
 echo
 echo
-OLD_AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 
 aws iam create-access-key > ${TMP_FILE}
+
+OLD_AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+OLD_AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+
 AWS_ACCESS_KEY_ID=$(jq -r .AccessKey.AccessKeyId ${TMP_FILE})
 AWS_SECRET_ACCESS_KEY=$(jq -r .AccessKey.SecretAccessKey ${TMP_FILE})
+
+if [[ ${AWS_ACCESS_KEY_ID} == ${OLD_AWS_ACCESS_KEY_ID} ]]; then
+    echo "Old and New keys are the same"
+fi
+
+if [[ ${AWS_SECRET_ACCESS_KEY} == ${OLD_AWS_SECRET_ACCESS_KEY} ]]; then
+    echo "Old and New Secret Keys are the same"
+fi
+
 echo "New ACCESS KEY ${AWS_ACCESS_KEY_ID}"
 
 echo "Removing previous secret"
