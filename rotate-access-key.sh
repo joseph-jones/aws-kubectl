b59b4182c7d3a3cc18a2c5e18b95b345a84ef4eb
Joseph Jones <the.tuxster@gmail.com>
2021-07-16 20:10:30 -0500

    make the script a little smarter

--- rotate-access-key.sh
+++ rotate-access-key.sh
@@ -8,16 +8,23 @@ set -o nounset
 TMP_FILE=$(mktemp)
 trap "rm -f ${TMP_FILE}" EXIT TERM INT
 
+echo "remove inactive access keys"
+INACTIVE=$(aws iam list-access-keys | jq '.AccessKeyMetadata[] | select(.Status != "Active") | .AccessKeyId')
+if [[ "x${INACTIVE:-}" -ne 'x' ]]; then
+    aws iam delete-access-key --access-key-id $INACTIVE
+fi
+
 OLD_AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 echo "Old ACCESS KEY ${OLD_AWS_ACCESS_KEY_ID}"
+
 
 aws iam create-access-key > ${TMP_FILE}
 AWS_ACCESS_KEY_ID=$(jq -r .AccessKey.AccessKeyId ${TMP_FILE})
 AWS_SECRET_ACCESS_KEY=$(jq -r .AccessKey.SecretAccessKey ${TMP_FILE})
 echo "New ACCESS KEY ${AWS_ACCESS_KEY_ID}"
 
-echo "Deleting ${OLD_AWS_ACCESS_KEY_ID}"
-aws iam delete-access-key --access-key-id $OLD_AWS_ACCESS_KEY_ID
+echo "Disabling old access key"
+aws iam update-access-key --access-key-id $OLD_AWS_ACCESS_KEY_ID --status Inactive
 
 echo "Removing previous secret"
 kubectl delete secret aws-secrets || true
